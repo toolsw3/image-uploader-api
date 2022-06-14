26febf7b0d66d1f1da030d06912bafea7287bf54
W3 Tools <53093889+toolsw3@users.noreply.github.com>
2022-06-14 16:54:23 -0300

    catch

--- app.py
+++ app.py
@@ -9,33 +9,38 @@ CORS(app, resources={r"*": {"origins": "*"}})
 
 @app.route('/upload', methods=["POST"])
 def index():
-    S3_ACCESS_KEY = os.environ.get('S3_ACCESS_KEY')
-    S3_SECRET_KEY = os.environ.get('S3_SECRET_KEY')
-    S3_FILES_BUCKET_NAME = os.environ.get('BUCKET_NAME')
-    LOCATION = os.environ.get('LOCATION')
-    PASSWORD = os.environ.get('PASSWORD')
+    try:
+        S3_ACCESS_KEY = os.environ.get('S3_ACCESS_KEY')
+        S3_SECRET_KEY = os.environ.get('S3_SECRET_KEY')
+        S3_FILES_BUCKET_NAME = os.environ.get('BUCKET_NAME')
+        LOCATION = os.environ.get('LOCATION')
+        PASSWORD = os.environ.get('PASSWORD')
 
-    if request.headers.get('password') != PASSWORD:
-        return jsonify({'status':'invalid password'})
+        if request.headers.get('password') != PASSWORD:
+            return jsonify({'status':'invalid password'})
+            
+        file = request.files['file']
         
-    file = request.files['file']
-    
-    if file is None:
-        return jsonify({'status':'NO-OK'})
+        if file is None:
+            return jsonify({'status':'NO-OK'})
 
-    s3 = boto3.client(
-            service_name="s3",
-            aws_access_key_id=S3_ACCESS_KEY,
-            aws_secret_access_key=S3_SECRET_KEY,
-        )
+        s3 = boto3.client(
+                service_name="s3",
+                aws_access_key_id=S3_ACCESS_KEY,
+                aws_secret_access_key=S3_SECRET_KEY,
+            )
 
-    filename = "{}_{}".format(random.getrandbits(128),file.filename)
+        filename = "{}_{}".format(random.getrandbits(128),file.filename)
 
-    s3.upload_fileobj(file, S3_FILES_BUCKET_NAME, filename)
-    
-    url = f"https://{S3_FILES_BUCKET_NAME}.s3.{LOCATION}.amazonaws.com/{filename}"
+        s3.upload_fileobj(file, S3_FILES_BUCKET_NAME, filename)
+        
+        url = f"https://{S3_FILES_BUCKET_NAME}.s3.{LOCATION}.amazonaws.com/{filename}"
 
-    return jsonify({'status':'OK', 'url': url})
+        return jsonify({'status':'OK', 'url': url})
+    except Exception as error:
+        print(str(error))
+        return jsonify({'status':'ERROR'})
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
